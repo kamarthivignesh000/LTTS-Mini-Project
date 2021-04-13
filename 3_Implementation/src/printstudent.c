4db35a3244ebe7bd4024c7a603bedf2b3a60ef25
vinaybc <65846052+vinaybc@users.noreply.github.com>
2021-04-13 18:55:25 +0530

    Create printstudent.c

---
+++ 3_Implementation/src/printstudent.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"prototypes.h"
+void printStudents(struct student s[])
+{
+	// Loop to iterate over the students
+	// students records
+	for (i = 0; i < n; i++) {
+
+		printf("Name of student %s \n",
+			s[i].name);
+		printf("Student roll number = %d \n",
+			s[i].rno);
+		printf("Student fees status = %c \n",
+			s[i].fees);
+		printf("Student number of days "
+			"present = %f \n",
+			s[i].days);
+		printf("Student attendence = %f \n",
+			s[i].attend);
+	}
+}
