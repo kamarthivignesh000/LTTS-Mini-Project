b7ee636a25fda770f9030447333cafb06bd7c1b7
vinaybc <65846052+vinaybc@users.noreply.github.com>
2021-04-13 19:12:09 +0530

    Update and rename srccode.c to main.c

---
+++ 3_Implementation/src/main.c
@@ -0,0 +1,29 @@
+#include"prototypes.h"
+
+int main()
+{
+	printf("Welcome to Student "
+		"database registration \n");
+
+	printf("Enter 0 to exit \n");
+	printf("Enter 1 to add student"
+		" record \n");
+
+	scanf("%d", &option);
+
+	
+	switch (option) {
+	case 0:
+		exit(0);
+
+	case 1:
+		add(s);
+		break;
+
+	default:
+		printf("Only enter 0 or 1");
+		execute();
+	}
+	return 0;
+}
+
