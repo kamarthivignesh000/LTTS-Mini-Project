680acfe01ab286d452e70ca1bf13844d4433245d
vinaybc <65846052+vinaybc@users.noreply.github.com>
2021-04-13 16:26:41 +0530

    Update prototypes.h

--- 3_Implementation/inc/prototypes.h
+++ 3_Implementation/inc/prototypes.h
@@ -1,9 +1,42 @@
+/**
+ * @file prototypes.h
+ * @author Vinay B C (https://github.com/vinaybc)
+ * @brief This file includes function prototypes, global variables and structure declaration
+ * @date 2021-04-08
+ * 
+ */
 #include<stdio.h>
-
+/**
+ * @brief Gets the data from user and updates the list of data
+ * 
+ * @param s 
+ */
 void add(struct student s[]);
+
+/**
+ * @brief shows the previous attendance percentage required for exams and get the data from the user and update the eligibility for the exams.
+ * 
+ * @param s 
+ */
 void eligibleStudents(struct student s[]);
+
+/**
+ * @brief shows the available choices for the software and will perform the below functionality using Switch Statements.
+ * 
+ */
 void execute();
+
+/**
+ * @brief  iterate over the list of students and print the details of the student.
+ * 
+ * @param s 
+ */
 void printStudents(struct student s[]);
+/**
+ * @brief gets the student roll number to delete the student record and update the student’s list.
+ * 
+ * @param s 
+ */
 void deleteRecord(struct student s[]);
 
 int option = 0;
@@ -14,6 +47,10 @@ float present = 75.00;
 char money = 'P';
 float tdays = 1;
 
+/**
+ * @brief Student structure (conatins the details about student)
+ * 
+ */
 struct student {
 	char name[20];
 	int rno;
