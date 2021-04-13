3b57d8fb067cd39ff17f53876ec4145fd750e9bd
vinaybc <65846052+vinaybc@users.noreply.github.com>
2021-04-13 18:53:40 +0530

    Create eligiblestudent.c

---
+++ 3_Implementation/src/eligiblestudent.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include"prototypes.h"
+
+void eligibleStudents(struct student s[])
+{
+	printf("________________"
+		"________________"
+		"_______________"
+		"_____________ \n");
+	printf("Qualified student are = \n");
+
+	// Iterate over the list
+	// of the students records
+	for (i = 0; i < n; i++) {
+		// Check for the eligibility
+		// of the student
+		if (s[i].fees == money || 'B' == money) {
+			if (s[i].attend >= present) {
+				printf("Student name = %s \n",
+					s[i].name);
+				printf("Student roll no. = %d \n",
+					s[i].rno);
+				printf(" Student fees = %c \n",
+					s[i].fees);
+				printf(" Student attendence = %f \n",
+					s[i].attend);
+			}
+		}
+	}
+}
