4a7d1f38d46d18c98347b2fd1bd3c0e8e99cded8
vinaybc <65846052+vinaybc@users.noreply.github.com>
2021-04-15 12:28:52 +0530

    Update student.h

--- 3_Implementation/inc/student.h
+++ 3_Implementation/inc/student.h
@@ -1,3 +1,13 @@
+/**
+ * @file student.h
+ * @author Vinay B C (https://github.com/vinaybc)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-15
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef _STUDENT_H_
 #define _STUDENT_H_
 
@@ -7,7 +17,10 @@
 
 #define MAX_NAME_LEN 50
 
-
+/**
+ * @brief Sturcute to store the student information
+ * 
+ */
 typedef struct {
     char name[MAX_NAME_LEN];
     int rno;
@@ -16,15 +29,39 @@ typedef struct {
     float attend;
 }Student;
 
+/**
+ * @brief Sturcture Node to traverse across each student 
+ * 
+ */
 struct Node {
     Student student;
     struct Node *next;
 };
 
+/**
+ * @brief To add student details 
+ * 
+ */
 void add();
+/**
+ * @brief To update the eligiblity of each students
+ * 
+ */
 void eligibleStudents();
+/**
+ * @brief To execute all other functions
+ * 
+ */
 void execute();
+/**
+ * @brief To print student details
+ * 
+ */
 void printStudents();
+/**
+ * @brief To delete student record
+ * 
+ */
 void deleteRecord();
 
 #endif
